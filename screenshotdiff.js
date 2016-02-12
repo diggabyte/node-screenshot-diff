a696358b5abd77d48d81145a01b5dd1c13ec4f05
diggabyte <scott@di.fm>
2016-02-12 23:53:13 +0000

    cleanup and coerce cli options

--- screenshotdiff.js
+++ screenshotdiff.js
@@ -3,6 +3,8 @@
 require('coffee-script');
 require('coffee-script/register');
 
+global.appRoot = require('path').resolve(__dirname);
+
 var screenshot = require('./lib/node-screenshot-diff/screenshot.js.coffee');
 var cli        = require('./lib/node-screenshot-diff/cli.js.coffee');
 
